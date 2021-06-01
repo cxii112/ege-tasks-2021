2f877429a92e2eafc4c725395fec49105ebb975d
cxii112 <lyakhovoy.d@ya.ru>
2021-06-01 19:19:04 +0500

    starting kp3764

---
+++ 26/kp3764_cpp/cmake-build-debug/CMakeFiles/kp3764_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kp3764_cpp.dir/main.cpp.obj"
+  "kp3764_cpp.exe"
+  "kp3764_cpp.exe.manifest"
+  "kp3764_cpp.lib"
+  "kp3764_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kp3764_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 26/kp3764_cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+# include <fstream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+int main() {
+
+
+    return EXIT_SUCCESS;
+}
