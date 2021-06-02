c022a2988fd5b316279c9646da68f7aef2f34cde
cxii112 <lyakhovoy-dmitry@users.noreply.github.com>
2021-06-02 15:48:19 +0500

    Use Leaf

--- 19-21/kp4181_cpp/main.cpp
+++ 19-21/kp4181_cpp/main.cpp
@@ -3,13 +3,16 @@
 #include "Leaf.h"
 
 int main() {
-    //        ( 0, 0)
-//    ( 0, 0)         ( 0, 0)
-//( 0, 0) ( 0, 0) ( 0, 0) ( 0, 0)
+//                            ( 0, 0)
+//            ( 0, 0)                         ( 0, 0)
+//    ( 0, 0)         ( 0, 0)         ( 0, 0)         ( 0, 0)
+//( 0, 0) ( 0, 0) ( 0, 0) ( 0, 0) ( 0, 0) ( 0, 0) ( 0, 0) ( 0, 0)
     Leaf *root = new Leaf(std::pair<int, int>(0, 0));
 
     Leaf *l1 = new Leaf(std::pair<int, int>(1,0), root);
     Leaf *l2 = new Leaf(std::pair<int, int>(0,1), root);
+    (*root).lChild(l1);
+    (*root).rChild(l2);
 
     std::cout << std::setfill(' ') << std::setw(5) << '(';
     std::cout << std::setw(2) << (*root).value().first;
