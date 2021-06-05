8ec62bc6d3ad5ef8e9e59e7cd7686015ffffadb0
cxii112 <lyakhovoy-dmitry@users.noreply.github.com>
2021-06-05 12:03:56 +0500

    Start solving 25.4120

---
+++ 25/kp4120_cpp/cmake-build-debug/CMakeFiles/kp4120_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kp4120_cpp.dir/main.cpp.o"
+  "kp4120_cpp"
+  "kp4120_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kp4120_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 25/kp4120_cpp/cmake-build-debug/CMakeFiles/kp4120_cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for kp4120_cpp.
+# This may be replaced when dependencies are built.
