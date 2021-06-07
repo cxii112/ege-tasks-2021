955bc6aa0ad28b4d2b9e35d8a7691f4ab0d59fcd
cxii112 <lyakhovoy-dmitry@users.noreply.github.com>
2021-06-08 01:40:18 +0500

    solve 25.3931

--- 25/kp3931_cpp/main.cpp
+++ 25/kp3931_cpp/main.cpp
@@ -12,6 +12,17 @@ int countDividers(int of) {
 }
 
 int main() {
-
+    const int START = 700000;
+    int len = 5;
+    int currDividersCount = 0;
+    int idx = START;
+    while (len > 0) {
+        if (countDividers(idx) > currDividersCount) {
+            currDividersCount = countDividers(idx);
+            std::cout << idx << ' ' << currDividersCount << std::endl;
+            len--;
+        }
+        idx++;
+    }
     return 0;
 }
