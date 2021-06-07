7c5eb5d0bcec6ac332489cfc6f8570c2c4617492
cxii112 <lyakhovoy-dmitry@users.noreply.github.com>
2021-06-07 20:18:30 +0500

    solving 25.3933

---
+++ 25/kp3933_cpp/cmake-build-debug/CMakeFiles/kp3933_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kp3933_cpp.dir/main.cpp.o"
+  "kp3933_cpp"
+  "kp3933_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kp3933_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 25/kp3933_cpp/cmake-build-debug/CMakeFiles/kp3933_cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for kp3933_cpp.
+# This may be replaced when dependencies are built.
