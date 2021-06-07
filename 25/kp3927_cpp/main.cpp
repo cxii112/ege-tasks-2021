fb916fc7db6edead1d23879d7ff75179d590e9bc
cxii112 <lyakhovoy-dmitry@users.noreply.github.com>
2021-06-08 04:42:36 +0500

    infinite loop

--- 25/kp3927_cpp/main.cpp
+++ 25/kp3927_cpp/main.cpp
@@ -8,7 +8,7 @@ void findPrimes(int upTo, std::vector<bool> *addTo) {
         (*addTo)[0] = (*addTo)[1] = false;
         for (unsigned long long check = 0; check < (*addTo).size(); check++) {
             if (!(*addTo)[check]) continue;
-            for (unsigned long long div = 2; div <= std::sqrt(upTo); div++) {
+            for (unsigned long long div = 2; div <= sqrt(upTo); div++) {
                 if (check % div == 0) (*addTo)[check] = false;
             }
         }
@@ -21,14 +21,30 @@ void findPrimes(int upTo, std::vector<bool> *addTo) {
     }
     for (unsigned long long check = PREV + 1; check < (*addTo).size(); check++) {
         if (!(*addTo)[check]) continue;
-        for (unsigned long long div = 2; div <= std::sqrt(upTo); div++) {
+        for (unsigned long long div = 2; div <= sqrt(upTo); div++) {
             if (check % div == 0) (*addTo)[check] = false;
         }
     }
 }
 
+int countDividers(int of) {
+    int result = 0;
+    for (int i = 1; i <= (int) sqrt(of); i++) {
+        if (of % i == 0) result++;
+    }
+    if ((int) sqrt(of) == sqrt(of)) --result;
+    return result;
+}
+
 int main() {
     const int DIVS_COUNT = 512;
     std::vector<bool> sieve;
+    int x = 1;
+    while (countDividers(++x) != DIVS_COUNT);
+    std::cout << x << ' ';
+    findPrimes(x, &sieve);
+    int d = (int) sqrt(x) + 1;
+    while (!sieve[--d]);
+    std::cout << ++d << std::endl;
     return 0;
 }
