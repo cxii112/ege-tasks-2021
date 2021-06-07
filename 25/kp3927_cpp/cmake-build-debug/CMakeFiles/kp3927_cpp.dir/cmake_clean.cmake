6e6475bb594e56c3527b6e63f93ebba9a686963d
cxii112 <lyakhovoy-dmitry@users.noreply.github.com>
2021-06-08 01:42:39 +0500

    solving 25.3927

---
+++ 25/kp3927_cpp/cmake-build-debug/CMakeFiles/kp3927_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kp3927_cpp.dir/main.cpp.o"
+  "kp3927_cpp"
+  "kp3927_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kp3927_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 25/kp3927_cpp/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+
+
+int main() {
+    const int DIVS_COUNT = 512;
+    return 0;
+}
